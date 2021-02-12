903d7ef9da66d2337c9aceac6729ae12cfbf3254
jayce <jayce404@foxmail.com>
2021-02-12 14:00:12 +0800

    完成增删改

--- springbootMyBatis/src/main/java/com/example/controller/BookController.java
+++ springbootMyBatis/src/main/java/com/example/controller/BookController.java
@@ -3,8 +3,7 @@ package com.example.controller;
 import com.example.pojo.Book;
 import com.example.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -26,5 +25,29 @@ public class BookController {
     @GetMapping("/getbookbyid")
     public String getBookById(Integer id){
         return bookService.getBookById(id).toString();
+    }
+
+    @RequestMapping(value = "/addbook", method = RequestMethod.POST)
+    public Map<String, Object> addBook(@RequestBody Book book) {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("success", (bookService.addBook(book) == 1) ? "OK" : "NO");
+
+        return map;
+    }
+
+    @RequestMapping(value = "/updatebook", method = RequestMethod.POST)
+    public Map<String, Object> updateBook(@RequestBody Book book) {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("success", (bookService.updateBook(book) == 1) ? "OK" : "NO");
+
+        return map;
+    }
+
+    @GetMapping("/deletebook")
+    public Map<String, Object> deleteBook(Integer id) {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("success", (bookService.deleteBookById(id) == 1) ? "OK" : "NO");
+
+        return map;
     }
 }
